ca9e9d699dd8738442e8e5d7d287a5c472ba6a48
Vladislav <vladplotnikov34@gmail.com>
2017-05-26 09:12:38 +0300

    Version 1.4

--- script.js
+++ script.js
@@ -13,12 +13,16 @@ $(document).on('change', '#time', function() {
 
 // РЕАЛИЗУЕМ ВЫПАДАЮЩЕЕ ОКОШЕЧКО
 $(document).on('click', '.mytable', function(){
+  $("#tableNumber").val($(this).html()); // При нажатии на объект с классом .mytable занести в невидимое поле по айди #tableNumber заносим текстовое наполнение нашего стола "Стол.."
+  $("#timeOfOrder").val($('#clock').html()); // заносим в невидимое поле с айди #timeOfOrder время считаное из слайдера по айди #clock
   $("#myModal").modal('show');
   $('.modal-title').html('Заказать ' + $(this).html());
 });
 
 // Код реаигирующий на нажатие кнопки подтвердить
-$(document).on('click', '#Confirmation', function() {
+// Подали в функцию, которая реагирует на нажатие перменную в которой хранится событие нажатия, с помощью этой переменной можно отменить это нажатие
+// эта отмена при помощии метода preventDefault
+$(document).on('click', '#Confirmation', function(e) {
   // Регулярки для каждого поля
   var nameRegexp = /^[A-ZА-Я][a-zа-я]+$/;
   var number1 = /^9[0-9]{2}$/;
@@ -30,36 +34,42 @@ $(document).on('click', '#Confirmation', function() {
   if (nameRegexp.test(name) == 0) {
     alert("Поле \"Имя\" введено неверено");
     $('#ClientName').val(''); // Очищаем  поле при помощи метода val
+    e.preventDefault(); // Если что то не верно введено - сработала валидация - мы обрываем пережачу на сервер
   }
 
   var surname = $('#ClientSurname').val();
   if (nameRegexp.test(surname) == 0) {
     alert("Поле \"Фамилия\" введено неверено");
     $('#ClientSurname').val(''); // Очищаем  поле при помощи метода val
+    e.preventDefault();
   }
 
   var num1 = $('#ClientNumber1').val();
   if (number1.test(num1) == 0) {
       alert("Первое поле телефонного номера введено неверено");
       $('#ClientNumber1').val(''); // Очищаем  поле при помощи метода val
+      e.preventDefault();
   }
 
   var num2 = $('#ClientNumber2').val();
   if (number2.test(num2) == 0) {
       alert("Второе поле телефонного номера введено неверено");
       $('#ClientNumber2').val(''); // Очищаем  поле при помощи метода val
+      e.preventDefault();
   }
 
   var num3 = $('#ClientNumber3').val();
   if (number34.test(num3) == 0) {
       alert("Третье поле телефонного номера введено неверено");
       $('#ClientNumber3').val(''); // Очищаем  поле при помощи метода val
+      e.preventDefault();
   }
 
   var num4 = $('#ClientNumber4').val();
   if (number34.test(num4) == 0) {
       alert("Четвертое поле телефонного номера введено неверено");
       $('#ClientNumber4').val(''); // Очищаем  поле при помощи метода val
+      e.preventDefault();
   }
 
   // Очищаем все поля при помощи метода val, который если получает что то в скобках, то НЕ ВОЗВРАЩАЕТ ЗНАЧЕНИЕ, а ПЕРЕНАЗНАЧАЕТ
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 app = express();
 var bodyParser = require("body-parser");
+var fs = require("fs"); // Подключили бибилиотеку-модуль для работы с файлами
 app.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -14,7 +15,16 @@ app.get("/", function(req,res){
 
 // Реализуем получение данных о клиенете с сайта на сервак
 app.post("/order", function(req,res){
-  console.log('name = ' + req.body.name + '\n surname = ' + req.body.surname + '\n number = ' + req.body.number1 + req.body.number2 + req.body.number3 + req.body.number4);
+  var text = 'name = ' + req.body.name + '\nsurname = ' + req.body.surname +
+  '\nnumber = ' + req.body.number1 + req.body.number2 +
+   req.body.number3 + req.body.number4 + "\n" + req.body.tableNumber + "\n" + req.body.timeOfOrder;
+   //Записываем данные полученныйе с сайта в файл
+   //Каждая новая бронь на сайте записывается в конец файла текстового при помощи функции appendFile из бибилиотеки fs
+   // 1 аргумент - имя файла, 2 - записываемый текст, 3 функция, которая вызовется при ошибке при записи
+  fs.appendFile("orders.txt",text, function(err){
+    if (err)
+      console.log('Obosrantus pri zapisiy v fail');
+  });
 })
 
 app.listen(7099);
